9342397fdebf87c9a6a24e0caa3782c773a8bce3
SyDs <setuclub@gmail.com>
2018-11-12 19:57:45 +0900

    pmx

--- DirectX12_2018/DirectX12_2018/PMX.cpp
+++ DirectX12_2018/DirectX12_2018/PMX.cpp
@@ -22,11 +22,12 @@ void PMX::Load()
 	char magic[4];
 	float version;
 	char datasize;
-	DataProp dataArray = {};//size = 8
+	PMXHeader pmxHeader = {};//size = 8
 
 	//モデル名関連
 	unsigned int namelength_jp;
-	wchar_t modelname_jp[3];//wchar_t = namelength / 2 = 3, char = namelength = 6
+	wchar_t dummyName[15] = {};
+	std::wstring modelname_jp;//wchar_t = namelength / 2 = 3, char = namelength = 6
 	unsigned int namelength_en;
 
 	//コメント関連
@@ -34,18 +35,13 @@ void PMX::Load()
 	wchar_t comment[25];//commentlength / 2
 	unsigned int commentlength_en;
 
-	//頂点情報関連
-	unsigned int vertexNum;
-	std::vector<PMXVertex> vertex;
 
-	//データの先を見るための配列
-	int checkData[50];
 
 	//pmxモデル情報
 	fread(&magic, sizeof(magic), 1, fp);
 	fread(&version, sizeof(version), 1, fp);
 	fread(&datasize, sizeof(datasize), 1, fp);
-	fread(&dataArray, sizeof(DataProp), 1, fp);
+	fread(&pmxHeader, sizeof(PMXHeader), 1, fp);
 
 	
 	//文字列サイズ取得
@@ -54,8 +50,9 @@ void PMX::Load()
 		for (int i = 0; i < namelength_jp / 2; ++i)
 		{
 			//日本語モデル名取得
-			fread(&modelname_jp[i], sizeof(modelname_jp[i]), 1, fp);
+			fread(&dummyName[i], 2, 1, fp);
 		}
+		modelname_jp = dummyName;
 	}
 
 	//文字列サイズ取得(英語)(響は英語名が入ってないのでサイズが0で読み込むモデル名配列もない)
@@ -75,6 +72,10 @@ void PMX::Load()
 	//英語コメント文字列取得(こちらも名前と同じく無い)
 	fread(&commentlength_en, sizeof(commentlength_en), 1, fp);
 
+	//頂点情報関連
+	unsigned int vertexNum;
+	std::vector<PMXVertex> vertex;
+
 	//頂点数取得
 	fread(&vertexNum, sizeof(vertexNum), 1, fp);
 	vertex.resize(vertexNum);
@@ -83,17 +84,130 @@ void PMX::Load()
 		//頂点情報読み込み
 		for (int i = 0; i < vertex.size(); ++i)
 		{
-			fread(&vertex[i], sizeof(PMXVertex), 1, fp);
+			//位置
+			fread(&vertex[i].pos, sizeof(vertex[i].pos), 1, fp);
+			//法線
+			fread(&vertex[i].normal, sizeof(vertex[i].normal), 1, fp);
+			//UV
+			fread(&vertex[i].uv, sizeof(vertex[i].uv), 1, fp);
+			//追加UV
+			if (pmxHeader.addUVNum != 0)
+			{
+				vertex[i].addUV.resize(pmxHeader.addUVNum);
+				for (int num = 0; num < pmxHeader.addUVNum; ++num)
+				{
+					fread(&vertex[i].addUV[num], sizeof(AdditionalUV), 1, fp);
+				}
+			}
+			//ウェイト方式
+			fread(&vertex[i].weightType, sizeof(vertex[i].weightType), 1, fp);
+			//ウェイト情報
+			if (vertex[i].weightType == 0)
+			{
+				//BDEF1
+				vertex[i].weightprop = {};
+
+				fread(&vertex[i].weightprop.index1, pmxHeader.vertexIndexSize, 1, fp);
+			}
+			else if (vertex[i].weightType == 1)
+			{
+				//BDEF2
+				vertex[i].weightprop = {};
+
+				fread(&vertex[i].weightprop.index1, pmxHeader.vertexIndexSize, 1, fp);
+				fread(&vertex[i].weightprop.index2, pmxHeader.vertexIndexSize, 1, fp);
+
+				fread(&vertex[i].weightprop.weight1, sizeof(float), 1, fp);
+				int a = 0;
+
+			}
+			else if (vertex[i].weightType == 2)
+			{
+				//BDEF4
+				vertex[i].weightprop = {};
+
+				fread(&vertex[i].weightprop.index1, pmxHeader.vertexIndexSize, 1, fp);
+				fread(&vertex[i].weightprop.index2, pmxHeader.vertexIndexSize, 1, fp);
+				fread(&vertex[i].weightprop.index3, pmxHeader.vertexIndexSize, 1, fp);
+				fread(&vertex[i].weightprop.index4, pmxHeader.vertexIndexSize, 1, fp);
+
+				fread(&vertex[i].weightprop.weight1, sizeof(vertex[i].weightprop.weight1), 1, fp);
+				fread(&vertex[i].weightprop.weight2, sizeof(vertex[i].weightprop.weight2), 1, fp);
+				fread(&vertex[i].weightprop.weight3, sizeof(vertex[i].weightprop.weight3), 1, fp);
+				fread(&vertex[i].weightprop.weight4, sizeof(vertex[i].weightprop.weight4), 1, fp);
+
+			}
+			else if (vertex[i].weightType == 3)
+			{
+				//SDEF
+				vertex[i].weightprop = {};
+				vertex[i].sdef = {};
+
+				fread(&vertex[i].weightprop.index1, pmxHeader.vertexIndexSize, 1, fp);
+				fread(&vertex[i].weightprop.index2, pmxHeader.vertexIndexSize, 1, fp);
+
+				fread(&vertex[i].weightprop.weight1, sizeof(vertex[i].weightprop.weight1), 1, fp);
+
+				fread(&vertex[i].sdef.C, sizeof(vertex[i].sdef.C), 1, fp);
+				fread(&vertex[i].sdef.R0, sizeof(vertex[i].sdef.R0), 1, fp);
+				fread(&vertex[i].sdef.R1, sizeof(vertex[i].sdef.R1), 1, fp);
+			}
+			//エッジ倍率
+			fread(&vertex[i].edgeScale, sizeof(vertex[i].edgeScale), 1, fp);
 		}
 	}
 
+	//面情報読み込み
+	unsigned int indexNum;
+
+	//インデックス数の取得
+	fread(&indexNum, sizeof(indexNum), 1, fp);
+	indexNum = indexNum / 3;//最大インデックス数 / 3頂点
+
+	//インデックス情報の取得
+	std::vector<PMXIndex> pmxIndex(indexNum);
 	{
-		for (int i = 0; i < 50; ++i)
+		for (int i = 0; i < pmxIndex.size(); ++i)
 		{
-			fread(&checkData[i], sizeof(checkData[i]), 1, fp);
+			fread(&pmxIndex[i].index1, pmxHeader.vertexIndexSize, 1, fp);
+			fread(&pmxIndex[i].index2, pmxHeader.vertexIndexSize, 1, fp);
+			fread(&pmxIndex[i].index3, pmxHeader.vertexIndexSize, 1, fp);
 		}
 	}
 
+
+	//テクスチャパスアドレスの読み込み
+	unsigned int materialNum;//マテリアル数
+	std::vector<std::wstring> materialName;//マテリアル名
+	const wchar_t* filepath = L"resource/model/";//ファイルアドレス
+	const wchar_t* slash = L"/";
+
+	//テクスチャ数の読み込み
+	fread(&materialNum, sizeof(materialNum), 1, fp);
+
+	//テクスチャ数分メモリを確保する
+	materialName.resize(materialNum);
 	
+	//テクスチャパスアドレス文字列の読み込み、組み合わせ
+	for (int i = 0; i < materialName.size(); ++i)
+	{
+		//テクスチャ名文字列数
+		unsigned int texNameSize = 0;
+		//テクスチャ名用ダミー変数
+		wchar_t dummy[20] = {};
+
+		//テクスチャ名文字列数読み込み
+		fread(&texNameSize, sizeof(texNameSize), 1, fp);
+		//テクスチャ名の読み込み
+		fread(&dummy, texNameSize, 1, fp);
+
+		//テクスチャアドレスの組み合わせ
+		materialName[i].append(filepath);
+		materialName[i].append(modelname_jp);
+		materialName[i].append(slash);
+		materialName[i].append(dummy);
+	}
+	
+
 	fclose(fp);
 }
--- DirectX12_2018/DirectX12_2018/PMX.h
+++ DirectX12_2018/DirectX12_2018/PMX.h
@@ -1,8 +1,31 @@
 #pragma once
 
 #include <vector>
+#include <map>
 
-struct DataProp
+enum class WeightPattern
+{
+	BDEF1,//: int | 4 | ボーンのみ
+	BDEF2,// : int, int, float | 4 * 3 | ボーン2つと、ボーン1のウェイト値(PMD方式)
+	BDEF4,// : int*4, float*4 | 4 * 8 | ボーン4つと、それぞれのウェイト値。ウェイト合計が1.0である保障はしない
+	SDEF,// : int, int, float, float3 * 3 | 4 * 12 | BDEF2に加え、SDEF用のfloat3(Vector3)が3つ。実際の計算ではさらに補正値の算出が必要(一応そのままBDEF2としても使用可能)
+};
+
+struct FLOAT3
+{
+	float x;
+	float y;
+	float z;
+};
+
+struct UV
+{
+	float u;
+	float v;
+};
+
+
+struct PMXHeader
 {
 	char encodeType;//エンコード方式  | 0:UTF16 1:UTF8
 	char addUVNum;//追加UV数 	| 0～4
@@ -14,22 +37,49 @@ struct DataProp
 	char rigidBodyIndexSize;//剛体Indexサイズ | 1,2,4 のいずれか
 };
 
-struct UV_EX {
+struct AdditionalUV {
 	float x;
 	float y;
 	float z;
 	float w;
 };
 
+struct PMDVertex_Weight
+{
+	int index1;
+	int index2;
+	int index3;
+	int index4;
+	float weight1;
+	float weight2;
+	float weight3;
+	float weight4;
+};
+
+struct SDEF_BoneInfo
+{
+	FLOAT3 C;
+	FLOAT3 R0;
+	FLOAT3 R1;
+};
+
 struct PMXVertex
 {
-	float pos[3];
-	float normal[3];
-	float uv[2];
-	std::vector<UV_EX> uv_ex;
-	char wait;
-	float edge;
+	FLOAT3 pos;//位置情報
+	FLOAT3 normal;//法線情報
+	UV uv;//UV情報
+	std::vector<AdditionalUV> addUV;//追加UV情報
+	uint8_t weightType;//ウェイト方式
+	PMDVertex_Weight weightprop;
+	SDEF_BoneInfo sdef;
+	float edgeScale;
+};
 
+struct PMXIndex
+{
+	unsigned int index1;
+	unsigned int index2;
+	unsigned int index3;
 };
 
 class PMX
